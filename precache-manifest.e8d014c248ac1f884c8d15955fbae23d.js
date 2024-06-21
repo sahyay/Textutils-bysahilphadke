d390628d7c8bd7e92d3c80c4d0fac8830d37acc9
sahyay <sahilphadke1612@gmail.com>
2024-06-21 17:31:22 +0530

    Updates

---
+++ precache-manifest.e8d014c248ac1f884c8d15955fbae23d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c88026023dd2c584ee44751a1f2910",
+    "url": "/Textutils-bysahilphadke/index.html"
+  },
+  {
+    "revision": "182e8aa44fcf1f95a7e8",
+    "url": "/Textutils-bysahilphadke/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "178a5f007b6a2c7c6a20",
+    "url": "/Textutils-bysahilphadke/static/js/2.12d3a3ae.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/Textutils-bysahilphadke/static/js/2.12d3a3ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4c22086003eed385c50",
+    "url": "/Textutils-bysahilphadke/static/js/3.b5bd36bd.chunk.js"
+  },
+  {
+    "revision": "182e8aa44fcf1f95a7e8",
+    "url": "/Textutils-bysahilphadke/static/js/main.ef380fd7.chunk.js"
+  },
+  {
+    "revision": "fae2dc34bd52f717e924",
+    "url": "/Textutils-bysahilphadke/static/js/runtime-main.c3507a37.js"
+  }
+]);
